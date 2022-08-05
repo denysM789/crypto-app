970f3cdc0906adb232eae86231f030bf4dc5c62f
Denys Mucker <denysm789@MacBook-Pro-von-Denys.fritz.box>
2022-08-05 17:08:05 +0200

    changed the getCoinData function

--- src/store/bottomChart/actions.js
+++ src/store/bottomChart/actions.js
@@ -5,18 +5,17 @@ import {
   GET_COINDATA_ERROR,
 } from "./index";
 
-export const getCoinData = (props) => async (dispatch, getState) => {
-  const state = getState();
-  const coinId = props[1];
-  const activeDuration = props[0];
+export const getCoinData =
+  (coinId, activeDuration) => async (dispatch, getState) => {
+    const state = getState();
 
-  try {
-    dispatch({ type: GET_COINDATA_PENDING });
-    const { data } = await axios(
-      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${activeDuration}&interval=daily`
-    );
-    dispatch({ type: GET_COINDATA_SUCCESS, payload: data });
-  } catch (err) {
-    dispatch({ type: GET_COINDATA_ERROR, payload: err });
-  }
-};
+    try {
+      dispatch({ type: GET_COINDATA_PENDING });
+      const { data } = await axios(
+        `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${activeDuration}&interval=daily`
+      );
+      dispatch({ type: GET_COINDATA_SUCCESS, payload: data });
+    } catch (err) {
+      dispatch({ type: GET_COINDATA_ERROR, payload: err });
+    }
+  };
